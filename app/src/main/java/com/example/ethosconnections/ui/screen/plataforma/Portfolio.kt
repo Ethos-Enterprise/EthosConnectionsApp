5035ec69c326e1a69678d661bd07775e4686f675
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2024-04-07 23:21:44 -0300

    fix: retirando o ComponentActivity da tela de Portfolio

--- app/src/main/java/com/example/ethosconnections/ui/screen/plataforma/Portfolio.kt
+++ app/src/main/java/com/example/ethosconnections/ui/screen/plataforma/Portfolio.kt
@@ -1,56 +1,30 @@
 package com.example.ethosconnections.ui.screen.plataforma
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.compose.AppTheme
 import com.example.ethosconnections.R
 import com.example.ethosconnections.ui.screen.plataforma.components.BoxEthos
-import com.example.ethosconnections.ui.screen.plataforma.ui.theme.EthosConnectionsTheme
 import com.example.ethosconnections.ui.theme.corLetra
 import com.example.ethosconnections.ui.theme.letraPadrao
-import com.example.ethosconnections.ui.theme.textoTop
 import com.example.ethosconnections.ui.theme.tituloConteudoAzul
 import com.example.ethosconnections.ui.theme.tituloConteudoBranco
 import com.example.ethosconnections.ui.theme.tituloPagina
-
-class Portfolio : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            EthosConnectionsTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Portfolio()
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun Portfolio(navController: NavController) {
