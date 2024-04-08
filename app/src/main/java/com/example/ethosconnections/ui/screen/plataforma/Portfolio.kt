6ce3dcddbe310dcf6e2f84a1a4a4560636ddaf34
Viviana Marca <viviana.marca@sptech.school>
2024-04-08 00:30:22 -0300

    inicicio componente Servico e adicionando novas variaveis de cor

--- app/src/main/java/com/example/ethosconnections/ui/screen/plataforma/Portfolio.kt
+++ app/src/main/java/com/example/ethosconnections/ui/screen/plataforma/Portfolio.kt
@@ -1,20 +1,14 @@
 package com.example.ethosconnections.ui.screen.plataforma
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -24,7 +18,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.compose.AppTheme
 import com.example.ethosconnections.R
 import com.example.ethosconnections.ui.screen.plataforma.components.BoxEthos
 import com.example.ethosconnections.ui.theme.corLetra
@@ -33,21 +26,6 @@ import com.example.ethosconnections.ui.theme.tituloConteudoAzul
 import com.example.ethosconnections.ui.theme.tituloConteudoBranco
 import com.example.ethosconnections.ui.theme.tituloPagina
 
-class Portfolio : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            AppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Portfolio()
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun Portfolio(navController: NavController) {
