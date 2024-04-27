d2f99357e59860afda5ed98f471e8da15b5a4709
VitXP <vitor.pereira@sptech.school>
2024-04-27 18:02:38 -0300

    Feat: Afdição tela de Pagamento

--- app/src/main/java/com/example/ethosconnections/ui/screen/plataforma/Pagamento.kt
+++ app/src/main/java/com/example/ethosconnections/ui/screen/plataforma/Pagamento.kt
@@ -1,12 +1,17 @@
 package com.example.ethosconnections.ui.screen.plataforma
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,18 +33,28 @@ fun Pagamento(navController: NavController) {
 
         BoxEthos {
             Column {
-                Spacer(modifier = Modifier.width(14.dp))
+                Spacer(modifier = Modifier.height(20.dp))
 
-                Image(
-                    modifier = Modifier.width(100.dp),
-                    painter = painterResource(id = R.mipmap.certo),
-                    contentDescription = "Icone de simbolo de certo",
-                )
-                Text(
-                    text = "Obrigado por fazer parte da Ethos!", style = tituloConteudoBranco
-                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center, // Centraliza horizontalmente
+                    verticalAlignment = Alignment.CenterVertically // Centraliza verticalmente
+                ) {
 
-                Spacer(modifier = Modifier.width(14.dp))
+                    Image(
+                        modifier = Modifier.size(28.dp),
+                        painter = painterResource(id = R.mipmap.certo),
+                        contentDescription = "Icone de símbolo de certo"
+                    )
+
+                    Spacer(modifier = Modifier.width(15.dp))
+
+                    Text(
+                        text = "Obrigado por fazer parte da Ethos!",
+                        style = tituloConteudoBranco
+                    )
+                }
             }
         }
     }
