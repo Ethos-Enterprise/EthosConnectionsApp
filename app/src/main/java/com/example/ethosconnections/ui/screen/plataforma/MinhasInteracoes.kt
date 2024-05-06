b66067d125598e3d5477bb24688a4216c9fb376d
VitXP <vitor.pereira@sptech.school>
2024-05-05 23:58:52 -0300

    Estrutura avaliacao

--- app/src/main/java/com/example/ethosconnections/ui/screen/plataforma/MinhasInteracoes.kt
+++ app/src/main/java/com/example/ethosconnections/ui/screen/plataforma/MinhasInteracoes.kt
@@ -1,12 +1,15 @@
 package com.example.ethosconnections.ui.screen.plataforma
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.ethosconnections.ui.screen.plataforma.components.BoxEthos
+import com.example.ethosconnections.ui.theme.tituloConteudoBranco
+import com.example.ethosconnections.ui.theme.tituloConteudoBrancoNegrito
 import com.example.ethosconnections.ui.theme.tituloPagina
 
 @Composable
@@ -16,6 +19,37 @@ fun MinhasInteracoes(navController: NavController){
         Text(text = "Minhas interações", style = tituloPagina)
 
         BoxEthos{
+            Text(text = "Categorias de interações", style = tituloConteudoBranco)
+
+        }
+
+        Text(text = "Empresas Contatadas", style = tituloPagina)
+
+        BoxEthos{
+
+            Row {
+
+
+                Row() {
+                    Text(text = "Em andamento: ", style = tituloConteudoBrancoNegrito)
+                    Text(text = "1 empresas", style = tituloConteudoBranco)
+
+                }
+
+                Row() {
+                    Text(text = "Finalizado: ", style = tituloConteudoBrancoNegrito)
+                    Text(text = "1 empresas", style = tituloConteudoBranco)
+
+                }
+            }
+        }
+
+        BoxEthos{
+            Text(text = "Box 1", style = tituloConteudoBranco)
+        }
+
+        BoxEthos{
+            Text(text = "Box 2", style = tituloConteudoBranco)
 
         }
     }
